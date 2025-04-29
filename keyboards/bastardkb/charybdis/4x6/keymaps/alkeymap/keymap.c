ee7bde698c181c4505f31d258e947489b61729d2
Alexander Ljungberg <aljungberg@wireload.net>
2025-04-29 10:21:38 +0000

    Globe key continued.

--- keyboards/bastardkb/charybdis/4x6/keymaps/alkeymap/keymap.c
+++ keyboards/bastardkb/charybdis/4x6/keymaps/alkeymap/keymap.c
@@ -83,7 +83,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
        KC_LCTL,    PT_Z,    KC_X,    KC_C,    KC_V,    KC_B,       KC_N,    KC_M, KC_COMM,  KC_DOT, PT_SLSH, KC_LALT,
   // ╰──────────────────────────────────────────────────────┤ ├──────────────────────────────────────────────────────╯
                                    KC_LGUI, KC_SPC,   LOWER,      RAISE,  KC_ENT,
-                                           KC_LALT, KC_BSPC,     KC_DEL
+                                           KC_LALT, KC_BSPC,     AP_GLOB
   //                            ╰───────────────────────────╯ ╰──────────────────╯
   ),
 
